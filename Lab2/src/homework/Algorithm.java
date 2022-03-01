7871250d479f9a02ad97b8374cc9c162e312d85a
iacobdariaroxana <iacobdariaroxana@gmail.com>
2022-03-01 19:01:59 +0200

    Create Algorithm.java

---
+++ Lab2/src/homework/Algorithm.java
@@ -0,0 +1,15 @@
+/** @author Iacob Daria-Roxana
+ * @since 2022
+ */
+package homework;
+
+/**
+ * Class for defining an algorithm
+ */
+public abstract class Algorithm {
+    /**
+     * Method used for finding a solution to a problem
+     * @return returns an object of type Solution
+     */
+    public abstract Solution solve();
+}
